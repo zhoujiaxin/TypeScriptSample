63564637c1aa68b53ac41fbd37370108819728a0
zhoujiaxin <zhoujiaxin1988@gmail.com>
2019-04-10 20:25:28 +0800

    更新示例代码

---
+++ sample_16_moduleresolution/moduleresolution_1_demo.ts
@@ -0,0 +1,5 @@
+/// <reference path="../sample_15_namespaces/validator/Validation.ts" />
+
+let msValidators: { [s: string]: Validation.StringValidator; } = {};
+
+import * as m14 from '../sample_14_modules/AllValidators';
